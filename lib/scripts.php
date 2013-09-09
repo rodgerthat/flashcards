887a64e97a10b2f246ef7d244bd6139572c4af8d
joelrnorris <joel.r.norris@gmail.com>
2013-09-08 19:56:48 -0500

    cleared out merge markup from scripts.php

--- lib/scripts.php
+++ lib/scripts.php
@@ -12,14 +12,9 @@ function fc_enqueue_scripts() {
     wp_register_script( 'flexslider', CHILD_URL.'/lib/js/jquery.flexslider-min.js', array('jquery'), '2.2.0', true );
     wp_enqueue_script( 'flexslider' );
 
-<<<<<<< HEAD
     // RESPONSIVE NAV
     wp_register_script( 'responsive-nav', CHILD_URL.'/lib/js/responsive-nav.min.js', array(), '1.0.2', true );
     wp_enqueue_script( 'responsive-nav' );
-=======
-    // FLASHCARDS
-//    wp_register_script( 'flashcards', CHILD_URL.'/lib/js/jquery.flashcards.js', array('jquery'), '1.0', true );
-//    wp_enqueue_script( 'flashcards' );
 
     // AJAX & INITS
     wp_register_script( 'fc', CHILD_URL.'/lib/js/jquery.fc.js', array('jquery', 'flexslider', 'responsive-nav'), '1.0', true );
